e2eebf0ff1a96c6b3a81375fd36173ede272322f
David <163066036+OneDavidd@users.noreply.github.com>
2024-04-19 21:34:08 -0300

    Add files via upload

---
+++ BusStats/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <title>BusStats</title>
+   <link rel="icon" href="fotos/bs.png">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body background="fotos/fundo2.jpg">
+
+   <script src="script.js"></script>
+</body>
+
+<footer>
+   <p>Copyright © OneDavidd - BusStats</p>
+</footer>
+
+</html>
+
+<?php ?>
