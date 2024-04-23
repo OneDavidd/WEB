34bd0d0d7bc09b5834eaeffb1b7b5859004c5754
David <163066036+OneDavidd@users.noreply.github.com>
2024-04-23 15:33:39 -0300

    Atlz BS

---
+++ BusStats/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <title>BusStats</title>
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:100" rel="stylesheet">
+   <link rel="icon" href="fotos/bs.png">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body style="background-image: url('fotos/fundo2.jpg');">
+<div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: -1;"></div>
+
+   <div class="titulo-busstats">
+      <h1>BusStats</h1>
+      <p>Este site encontra-se em manutenção</p>
+   </div>
+
+   <div class="aba-lateral">
+        <div class="logo-topo">
+            <a href="http://localhost/busstats/index.php">
+            <img src="fotos/bs.png" alt="Logo Superior" width="100">
+        </div>
+
+        <div>
+            <a href="fornecedores.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 1" width="36">
+                Fornecedores
+            </a>
+            <a href="empresas.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 2" width="36">
+                Empresas
+            </a>
+            <a href="categorias.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 3" width="36">
+                Categorias
+            </a>
+            <a href="fabricantes.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 4" width="36">
+                Fabricantes
+            </a>
+            <a href="curiosidades.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 5" width="36">
+                Curiosidades
+            </a>
+            <a href="atualizacoes.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 6" width="36">
+                Atualizações
+            </a>
+            <a href="https://www.urbs.curitiba.pr.gov.br/" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 7" width="36">
+                URBS
+            </a>
+            <a href="https://www.curitiba.pr.gov.br/" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 8" width="36">
+                Prefeitura de Curitiba
+            </a>
+        </div>
+
+        <div class="texto-final-aba">
+            <p>Curitiba - PR Abril, 2024</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+   <script src="script.js"></script>
+</body>
+
+<footer>
+   <p>Copyright © OneDavidd - BusStats</p>
+</footer>
+
+</html>
+
+<?php ?>
