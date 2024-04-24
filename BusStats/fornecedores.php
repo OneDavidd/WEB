43391ac49c2b0f2d662a5d943b88a68d645aef02
David <163066036+OneDavidd@users.noreply.github.com>
2024-04-23 21:29:58 -0300

    Atualização BS

---
+++ BusStats/fornecedores.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <title>Fornecedores</title>
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:100" rel="stylesheet">
+   <link rel="icon" href="fotos/bs.png">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body style="background-image: url('fotos/fundo3.jpg');">
+    <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: -1;"></div>
+
+   <div class="aba-lateral">
+        <div class="logo-topo">
+            <a href="http://localhost/busstats/index.php">
+            <img src="fotos/bs.png" alt="Logo Superior" width="100">
+        </div>
+
+        <div>
+            <a href="fornecedores.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 1" width="36">
+                Fornecedores
+            </a>
+            <a href="empresas.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 2" width="36">
+                Empresas
+            </a>
+            <a href="categorias.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 3" width="36">
+                Categorias
+            </a>
+            <a href="fabricantes.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 4" width="36">
+                Fabricantes
+            </a>
+            <a href="curiosidades.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 5" width="36">
+                Curiosidades
+            </a>
+            <a href="atualizacoes.php" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 6" width="36">
+                Atualizações
+            </a>
+            <a href="https://www.urbs.curitiba.pr.gov.br/" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 7" width="36">
+                URBS
+            </a>
+            <a href="https://www.curitiba.pr.gov.br/" class="menu-item">
+                <img src="fotos/bs.png" alt="Logo Opção 8" width="36">
+                Prefeitura de Curitiba
+            </a>
+        </div>
+
+        <div class="texto-final-aba">
+            <p>Curitiba - PR Abril, 2024</p>
+        </div>
+    </div>
+
+    <div class="image-gallery">
+        <a href="https://www.scania.com/br/pt/home.html" class="image-link">
+        <img src="fotos/scania.png" alt="Imagem Scania" class="image-item">
+            <p>Scania</p>
+        </a>
+        <a href="https://www2.mercedes-benz.com.br/" class="image-link">
+        <img src="fotos/mercedes.png" alt="Imagem Mercedes" class="image-item">
+            <p>Mercedes-Benz</p>
+        </a>
+        <a href="https://www.volvogroup.com/br/" class="image-link">
+        <img src="fotos/volvo.png" alt="Imagem Volvo" class="image-item">
+            <p>Volvo</p>
+        </a>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+   <script src="script.js"></script>
+</body>
+
+<footer>
+   <p>Copyright © OneDavidd - BusStats</p>
+</footer>
+
+</html>
+
+<?php ?>
